6e58486fcc13c4d705532acb2c078657294d8536
Ayan Ahmad <chaudharyayan100@gmail.com>
2025-08-14 01:26:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio_demo/", // Replace with your repo name
+  base: "/Portfolio_Demo/", // exact repo name, matching case
 });
